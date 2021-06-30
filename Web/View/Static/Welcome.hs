ca536635457854c327cc45a23b49d7db064056fd
Damiano Presa <tolarianwiz@gmail.com>
2021-06-30 19:24:31 +0200

    add posts to postgres

--- Web/View/Static/Welcome.hs
+++ Web/View/Static/Welcome.hs
@@ -1,10 +1,12 @@
 module Web.View.Static.Welcome where
+
 import Web.View.Prelude
 
 data WelcomeView = WelcomeView
 
 instance View WelcomeView where
-    html WelcomeView = [hsx|
+  html WelcomeView =
+    [hsx|
          <div style="background-color: #657b83; padding-top: 2rem; padding-bottom: 2rem; color:hsla(196, 13%, 96%, 1); border-radius: 4px">
               <div style="max-width: 800px; margin-left: auto; margin-right: auto">
                   <h1 style="margin-bottom: 2rem; font-size: 2rem; font-weight: 300; border-bottom: 1px solid white; padding-bottom: 0.25rem; border-color: hsla(196, 13%, 60%, 1)">
@@ -12,7 +14,7 @@ instance View WelcomeView where
                   </h1>
 
                   <h2 style="margin-top: 0; margin-bottom: 0rem; font-weight: 900; font-size: 3rem">
-                      It's working!
+                      Hey there!
                   </h2>
 
                   <p style="margin-top: 1rem; font-size: 1.75rem; font-weight: 600; color:hsla(196, 13%, 80%, 1)">
